56baa779f68932548d480ffb82ff62ff18b81e02
Raihan Firdaus <raihan22sxilex23@gmail.com>
2025-05-04 23:19:46 +0700

    modified:   src/app/blog/[objectId]/page.tsx

--- src/app/blog/[objectId]/page.tsx
+++ src/app/blog/[objectId]/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 
-export default async function FilledCharacters({
-  params,
-}: {
-  params: { objectId: string };
-}) {
+type Props = {
+  params: {
+    objectId: string;
+  };
+};
+
+export default async function FilledCharacters({ params }: Props) {
   const { objectId } = params;
 
   const res = await fetch(
@@ -24,50 +26,47 @@ export default async function FilledCharacters({
     genderCategory: { gender: string };
   } = await res.json();
 
-  console.log(data);
   return (
-    <>
-      <main>
-        <section className="mx-auto w-screen min-h-screen bg-black">
-          <div className="w-screen h-screen relative overflow-hidden">
-            <Image
-              src={data.background}
-              alt="content-background"
-              width={10000}
-              height={10000}
-              className="object-cover opacity-40"
-            />
-            <div className="grid grid-cols-2 justify-between top-16 absolute">
-              <div className="w-[86%] max-h-screen  relative my-6 mr-6 ml-10">
-                <Image
-                  src={data.characterImage}
-                  alt="Hero Image"
-                  width={100000}
-                  height={100000}
-                  className="object-cover"
-                />
-              </div>
-              <div className="px-8 pt text-center flex flex-col gap-18 justify-center place-items-center">
-                <h1 className="mt-2 w-full min-h-10 text-7xl text-yellow-300 font-extrabold">
-                  {data.characters}
-                </h1>
-                <span
-                  className={`${
-                    data.genderCategory?.gender === "Male"
-                      ? "bg-blue-500 text-white"
-                      : "bg-amber-300"
-                  } text-lg font-mono border border-black w-fit px-4 bg-amber-300 text-white rounded-3xl py-2`}
-                >
-                  {data.genderCategory?.gender ?? "Unknown"}
-                </span>
-                <p className=" text-yellow-200 text-2xl  mt-[4px]">
-                  {data.fullDescription}
-                </p>
-              </div>
+    <main>
+      <section className="mx-auto w-screen min-h-screen bg-black">
+        <div className="w-screen h-screen relative overflow-hidden">
+          <Image
+            src={data.background}
+            alt="content-background"
+            width={10000}
+            height={10000}
+            className="object-cover opacity-40"
+          />
+          <div className="grid grid-cols-2 justify-between top-16 absolute">
+            <div className="w-[86%] max-h-screen relative my-6 mr-6 ml-10">
+              <Image
+                src={data.characterImage}
+                alt="Hero Image"
+                width={100000}
+                height={100000}
+                className="object-cover"
+              />
+            </div>
+            <div className="px-8 text-center flex flex-col gap-18 justify-center place-items-center">
+              <h1 className="mt-2 w-full min-h-10 text-7xl text-yellow-300 font-extrabold">
+                {data.characters}
+              </h1>
+              <span
+                className={`${
+                  data.genderCategory?.gender === "Male"
+                    ? "bg-blue-500 text-white"
+                    : "bg-amber-300"
+                } text-lg font-mono border border-black w-fit px-4 rounded-3xl py-2`}
+              >
+                {data.genderCategory?.gender ?? "Unknown"}
+              </span>
+              <p className="text-yellow-200 text-2xl mt-[4px]">
+                {data.fullDescription}
+              </p>
             </div>
           </div>
-        </section>
-      </main>
-    </>
+        </div>
+      </section>
+    </main>
   );
 }
